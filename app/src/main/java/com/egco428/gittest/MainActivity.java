274d6cfab46388a349067fddae4754d8aa41ef66
Walaitorn <33399pam@gmail.com>
2016-09-01 12:05:20 +0700

    -change image -back button -toast

--- app/src/main/java/com/egco428/gittest/MainActivity.java
+++ app/src/main/java/com/egco428/gittest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.egco428.gittest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +9,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imageView;
+    private boolean flag = true;
+    public final static String EXTRA_MESSAGE = "com.egco428.MESSAGE";
+    private int msg = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        imageView = (ImageView)findViewById(R.id.imageView);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,12 +35,46 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
+
+    public void sendMethod(MenuItem item) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        finish();
+    }
+
+    public void Changeimg(MenuItem item) {
+
+        if (msg % 2 == 0) {
+            imageView.setImageResource(R.drawable.logo1);
+            msg = msg-1;
+        } else if (msg % 2 != 0){
+            imageView.setImageResource(R.drawable.logo2);
+            msg = msg+1;
+    }
+
+}
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.add(Menu.NONE,Menu.NONE, 104,"New Toast");
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);//if toolbar has enough space
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Toast.makeText(MainActivity.this,"new message here",Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
         return true;
     }
 
